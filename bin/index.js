2f59b7969ab7944e5b6a284d53f893b8a4e718fe
Joe Doiron <jdoiron2@unum.com>
2021-10-22 10:05:29 -0400

    required path, added comments and checks

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,53 @@
 #! /usr/bin/env node
 
+// import all required modules
+const path = require('path')
 const node_modules = require('node_modules-path');
 const yargs = require("yargs");
 const marked = require("marked");
 const hljs   = require('highlight.js');
 const fs = require('fs');
+// get the key used to call index.js
 const package_json = require('../package.json');
+const command_name = Object.keys(package_json.bin)[0]
 
+function fileExists(filename) {
+    try {
+      fs.accessSync(filename, fs.R_OK);
+      return true;
+    } catch (e) {
+      return false;
+    }
+}
+
+// setup CLI
 var argv = yargs(process.argv.slice(2))
-    .usage(`Usage: ${Object.keys(package_json.bin)[0]} -f [markdown file] -o [html file] -s [style to use]`)
+    .usage(`Usage: ${command_name} -f [markdown file] -o [html file] -s [style to use]`)
     .example(
-        `${Object.keys(package_json.bin)[0]} -f foo.md -o foo.html -s light`, 
+        `${command_name} -f foo.md -o foo.html -s light`, 
         'compile foo.md to HTML, outputting the result to foo.html.')
     .alias('f', 'file').nargs('f', 1).describe('f', 'load a file')
     .alias('o', 'output').nargs('o', 1).describe('o', 'specify file to output, including .html')
     .alias('s', 'style').describe('s', 'style to use').choices('s', ["light", "dark"])
-    .demandOption(['f'])
+    .demandOption(['f', 'o'])
     .help('h')
     .alias('h', 'help')
     .epilog('copyright 2021')
+    .check((argv) => {
+        if (fileExists(argv.file)) {
+           return true;
+        }
+        throw new Error('Error: filepath is not a readable file');
+    })
+    .check((argv) => {
+        if (path.extname(argv.output) === ".html") {
+            return true;
+        }
+        throw new Error('Error: output must be an html file');
+    })
     .argv;
 
+// set marked options for using highlight.js for code highlighting
 marked.setOptions({
     renderer: new marked.Renderer(),
     highlight: function(code, lang) {
@@ -37,6 +64,7 @@ marked.setOptions({
     xhtml: false
 });
 
+// define styling based on user input
 if (argv.style === "light" || argv.style === "dark") {
     var style_file = `github-markdown-${argv.style}.css`;
     if (argv.style === "light") {
@@ -53,7 +81,8 @@ const node_modules_path = node_modules();
 const markdownString = fs.readFileSync(argv.file,"utf8");
 const github_style = fs.readFileSync(`${node_modules_path}\\github-markdown-css\\${style_file}`, "utf8");
 const code_style = fs.readFileSync(`${node_modules_path}\\highlight.js\\styles\\${code_highlight_file}`, "utf8");
-const doc = `
+
+const html = `
 <!DOCTYPE html>
 <style>
     html {
@@ -79,7 +108,8 @@ const doc = `
 	${marked(markdownString)}
 </article>`
 
-fs.writeFileSync(argv.output,  doc);
+fs.writeFileSync(argv.output,  html);
+console.log(`Success: ${argv.file} -> ${argv.output}`);
 
 
 
